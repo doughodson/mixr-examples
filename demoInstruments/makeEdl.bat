b90d0878f018c468590a7aa45221e4e0db8b7479
Doug Hodson <doug@openeaagles.org>
2017-06-08 09:29:06 -0400

    updated make edl files

--- demoInstruments/makeEdl.bat
+++ demoInstruments/makeEdl.bat
@@ -2,6 +2,6 @@ rem
 call ..\makedefs.bat
 rem
 %mcpp% configs/testinstruments.epp > testinstruments.edl ^
- -I ..\..\openeaagles\include ^
+ -I ..\..\mxrp\include ^
  -I ..\shared\data ^
  -DFONT_PATH="\"../shared/data/fonts/\"

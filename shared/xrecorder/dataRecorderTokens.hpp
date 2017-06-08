15471e064c0b0f38891b0fa942091ac6911e1a5e
Doug Hodson <doug@openeaagles.org>
2017-06-08 17:15:19 -0400

    update

--- shared/xrecorder/dataRecorderTokens.hpp
+++ shared/xrecorder/dataRecorderTokens.hpp
@@ -7,7 +7,7 @@
 //
 //    2) P1 to P4 are the required objects passed to the SAMPLE macro, and
 //       V1 to V4 are the required values passed to the SAMPLE macros.
-//       (see "openeaagles/simulation/DataRecorder.h")
+//       (see "mxrp/simulation/DataRecorder.hpp")
 //
 //------------------------------------------------------------------------------
 #ifndef __oe_xrecorder_Id_Tokens_H__

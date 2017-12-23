dc9dde414009f7f4db203cd17524d045b75ea0ef
Doug Hodson <doug@openeaagles.org>
2017-12-23 12:08:02 -0500

    renamed all makeedl.cmd files to make-edl.cmd

--- make-edl.cmd
+++ make-edl.cmd
@@ -5,32 +5,32 @@
 
 for /D %%G IN (demo*) do (
    pushd %%G
-   if EXIST makeedl.cmd (
-        call makeedl.cmd
+   if EXIST make-edl.cmd (
+        call make-edl.cmd
       )
    popd
 )
 
 for /D %%G IN (main*) do (
    pushd %%G
-   if EXIST makeedl.cmd (
-        call makeedl.cmd
+   if EXIST make-edl.cmd (
+        call make-edl.cmd
       )
    popd
 )
 
 for /D %%G IN (test*) do (
    pushd %%G
-   if EXIST makeedl.cmd (
-        call makeedl.cmd
+   if EXIST make-edl.cmd (
+        call make-edl.cmd
       )
    popd
 )
 
 for /D %%G IN (tutorial*) do (
    pushd %%G
-   if EXIST makeedl.cmd (
-        call makeedl.cmd
+   if EXIST make-edl.cmd (
+        call make-edl.cmd
       )
    popd
 )

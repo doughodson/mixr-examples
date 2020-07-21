90e97adb5d2c217a688845ba25ec170fbf2ccaa6
Doug Hodson <doug@sidechannel.net>
2020-07-21 17:54:17 -0400

    updated make-edl for mainGlut example

--- mainGlut/make-edl.cmd
+++ mainGlut/make-edl.cmd
@@ -1,4 +1,4 @@
 ::
 call ..\makedefs.cmd
 %mcpp% configs/alignmentPattern.epp > alignmentPattern.edl %EPPFLAGS%
-%mcpp% configs/testDisplay.epp > testDisplay.edl %EPPFLAGS%
+%mcpp% configs/textDisplay.epp > textDisplay.edl %EPPFLAGS%

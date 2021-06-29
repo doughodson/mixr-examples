41df0332f363413bafdfc4513a8d9bc96dc79330
Doug Hodson <doug@sidechannel.net>
2021-06-29 15:31:31 -0400

    updated test linear system example

--- testLinearSys/main.cpp
+++ testLinearSys/main.cpp
@@ -1,5 +1,5 @@
 
-#include "mixr/linearsystem/Sz1.hpp"
+#include "mixr/base/linearsystem/Sz1.hpp"
 #include <iostream>
 #include <cstdlib>
 #include <cmath>
@@ -11,7 +11,7 @@ int main(int, char**)
    const int RATE{100};
    const double A{10.0};
 
-   mixr::linearsystem::Sz1 filter;
+   mixr::base::Sz1 filter;
 
    filter.setN1(0.0);
    filter.setN2(1.0);

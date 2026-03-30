e7b5e1c6b38c9e8a180ce87903da84e6344d7090
Doug Hodson <doug@sidechannel.net>
2026-03-30 09:22:21 -0400

    updated premake script

--- build/premake/run-premake.bat
+++ build/premake/run-premake.bat
@@ -2,8 +2,8 @@ rem
 rem delete old solution/workspace files
 rem
 rmdir /S /Q ..\gmake
-rmdir /S /Q ..\vs2019
 rmdir /S /Q ..\vs2022
+rmdir /S /Q ..\vs2026
 rmdir /S /Q ..\obj
 rem
 rem delete old shared example libraries
@@ -16,5 +16,5 @@ del ..\..\lib\*.lib
 rem
 rem create solution/workspace files
 rem
-premake5-beta7.exe --os=windows --file=mixr-examples.lua vs2019
-premake5-beta7.exe --os=windows --file=mixr-examples.lua vs2022
+premake5-beta8.exe --os=windows --file=mixr-examples.lua vs2022
+premake5-beta8.exe --os=windows --file=mixr-examples.lua vs2026

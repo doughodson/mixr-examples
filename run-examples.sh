4ccea4cba4e94351ae6b0312b8dc00004638071e
Doug Hodson <doug@sidechannel.net>
2026-04-13 10:18:24 -0400

    added new script to launch all examples

---
+++ run-examples.sh
@@ -0,0 +1,19 @@
+
+source setenv.sh
+echo "demoEfis"
+cd demoEfis
+./demoEfis glutdisplay.edl
+echo "demoFlightDisplay1"
+cd ../demoFlightDisplays1
+./demoFlightDisplays1 test.edl
+echo "demoFlightDisplay2"
+cd ../demoFlightDisplays2
+./demoFlightDisplays2 test.edl
+echo "demoInstruments"
+cd ../demoInstruments
+./demoInstruments testinstruments.edl
+echo "demoSubDisplays"
+cd ../demoSubDisplays
+./demoSubDisplays test.edl
+
+

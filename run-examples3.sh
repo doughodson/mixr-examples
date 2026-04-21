9205276671bfdc28c311549244b18e93c64724be
Doug Hodson <doug@sidechannel.net>
2026-04-21 14:37:08 -0400

    update

--- run-examples3.sh
+++ run-examples3.sh
@@ -2,4 +2,18 @@ source setenv.sh
 cd testEvents
 echo "testEvents -f testEvents.edl"
 ./testEvents -f testEvents.edl
+cd ../testGraphics
+echo "testGraphics -f test1.edl"
+./testGraphics -f test1.edl
+echo "testGraphics -f test2.edl"
+./testGraphics -f test2.edl
+cd ../testInfrared
+echo "testInfrared -f test1.edl"
+./testInfrared -f test1.edl
+cd ../testLinearSys
+echo "testLinearSys"
+./testLinearSys
+cd ../testLinkage
+echo "testLinkage -f test1.edl"
+./testLinkage -f test1.edl
 

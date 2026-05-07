07cf3008989975317a2d7e9a4c863cd4897aef9a
Doug Hodson <doug@sidechannel.net>
2026-05-07 19:41:49 -0400

    update

--- run-examples1.sh
+++ run-examples1.sh
@@ -13,17 +13,7 @@ cd mainNonRT1;          make run; cd ..
 cd mainNonRT2;          make run; cd ..
 cd mainPuzzle1;         make run; cd ..
 cd mainPuzzle2;         make run; cd ..
-#cd ../mainSim1
-#echo "mainSim1 -f test0.edl"
-#./mainSim1 -f test0.edl
-#echo "mainSim1 -f test1.edl"
-#./mainSim1 -f test1.edl
-#echo "mainSim1 -f test2.edl"
-#./mainSim1 -f test2.edl
-#echo "mainSim1 -f test3.edl"
-#./mainSim1 -f test3.edl
-#echo "mainSim1 -f test4.edl"
-#./mainSim1 -f test4.edl
+cd mainSim1;            make run; cd ..
 #cd ../mainSim2
 #echo "mainSim2 -f test0.edl"
 #./mainSim2 -f test0.edl
